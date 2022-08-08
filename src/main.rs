469f33483d61f065b876ea1b9a12d8e891bd4330
sasanzare <sasanzare75@gmail.com>
2022-08-08 19:24:07 +0430

    Add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #[macro_use] extern crate rocket;
 
 
+use rocket::{get, http::Status, serde::json::Json};
+
+#[get("/")]
+fn index() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello")))
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
-}
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
 }
